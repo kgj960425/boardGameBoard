e3e5b2ed69b10805f6a61b308e3a0d1991bf1ec1
kgj960425 <kgj960425@google.com>
2025-03-14 16:50:43 +0900

    화면 분리

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,8 +1,17 @@
+import Navbar from "./Navbar";
+
 function LandingPage() {
     return (
-        <div>
-            LandingPage
-        </div>
+        <>
+            <div>
+                LandingPage
+            </div>
+            {/*일단 
+            1) 랜딩 페이지는 접속하면 무조건 보이도록 빈 페이지로 만들어 놓고
+            2) 로그인 페이지로 이동하는 버튼을 따로 만들어 놓는다.
+            3) Navbar는 항상 페이지 상단에 노출되도록 할 것. 
+            */}
+        </> 
     )
 }
 
